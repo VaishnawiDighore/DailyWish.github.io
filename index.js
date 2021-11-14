443530bb692d01febe345f977f85aa973663baed
VaishnawiDighore <92801895+VaishnawiDighore@users.noreply.github.com>
2021-11-14 14:23:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function morning() {
+    var fM = document.getElementById("p");
+    fM.innerText = "Good Morning!  ";
+    fM.style.color = "blue";
+    var styleM = document.getElementById("p1");
+    styleM.style.color = "yellow";
+  }
+  function afternoon() {
+    var fA = document.getElementById("p");
+    fA.innerText = "Good Afternoon!  ";
+  }
+  function night() {
+    var fE = document.getElementById("p");
+    fE.innerText = "Good Night!  ";
+  }
